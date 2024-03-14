7b5161db905b7e3e2ab3829fdad434fc76e34474
Sohom <sohommondalofficial@gmail.com>
2024-03-15 01:17:09 +0530

    ref: input type added

--- components/chat-input.tsx
+++ components/chat-input.tsx
@@ -42,7 +42,8 @@ const ChatInput = () => {
   return (
     <div className="p-5">
       <Input
-        placeholder="send message"
+        type="text"
+        placeholder="Send Message 💬🚀"
         onKeyDown={(e) => {
           if (e.key === "Enter") {
             handleSendMessage(e.currentTarget.value);
