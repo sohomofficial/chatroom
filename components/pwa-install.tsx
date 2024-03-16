035854dbe2eec751e4a51c2d64ebf731beaec1a9
Sohom <sohommondalofficial@gmail.com>
2024-03-16 14:36:46 +0530

    minor change: pwa download button

--- components/pwa-install.tsx
+++ components/pwa-install.tsx
@@ -1,16 +1,21 @@
 "use client";
 import { useEffect, useState } from "react";
 import { Button } from "./ui/button";
-import { DownloadCloud } from "lucide-react";
+import { Download } from "lucide-react";
 
 const InstallPWA = () => {
+  const [showButton, setShowButton] = useState<Boolean>(false);
   const [prompt, setPrompt] = useState<any>(null);
   useEffect(() => {
     const handleBeforeInstallPrompt = (event: any) => {
       event.preventDefault();
       setPrompt(event);
+      if (window.matchMedia("(display-mode: standalone)").matches) {
+        setShowButton(true);
+      }
     };
     window.addEventListener("beforeinstallprompt", handleBeforeInstallPrompt);
+
     return () => {
       window.removeEventListener(
         "beforeinstallprompt",
@@ -26,9 +31,13 @@ const InstallPWA = () => {
   };
 
   return (
-    <Button size={"icon"} onClick={handleInstallClick}>
-      <DownloadCloud className="h-4 w-4" />
-    </Button>
+    <div>
+      {showButton && (
+        <Button size={"icon"} onClick={handleInstallClick}>
+          <Download className="h-4 w-4" />
+        </Button>
+      )}
+    </div>
   );
 };
 
